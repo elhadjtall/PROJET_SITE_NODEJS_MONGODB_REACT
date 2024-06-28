0b8b64aede07049cf1b0f9c347c0ab1fff95d554
unknown <elhadjoumartall10@gmail.com>
2024-06-28 11:46:48 +0200

    modification du connexion

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const cors = require('cors');  // Ajout de cors
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000;
@@ -13,7 +13,7 @@ app.use(express.json());
 // DB Connection
 console.log("DB user name ", process.env.DB_USER);
 
-// MongoDB Atlas Connection 
+// MongoDB Atlas Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?appName=Project-Site`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,7 +38,7 @@ async function run() {
     const userCollections = database.collection("users");
     const classesCollections = database.collection("classes");
     const cartCollections = database.collection("cart");
-    const paymantCollections = database.collection("paymants");
+    const paymentCollections = database.collection("payments");
     const enrolledCollections = database.collection("enrolled");
     const appliedCollections = database.collection("applied");
 
@@ -51,66 +51,65 @@ async function run() {
       res.send(result);
     });
 
-    //La requête pour récuperer les données
+    // La requête pour récuperer les données
     app.get('/classes', async (req, res) => {
-      const query = {status: "approved"};
+      const query = { status: "approved" };
       const result = await classesCollections.find(query).toArray();
       res.send(result);
-  })
-   // Get class by instrucor email address
-   app.get('/classes/:email', async (req, res) => {
-      const email = req.param.email;
-      const query = {instructorEmail: email};
-      const result = await classesCollections.find(query).toArray();
-      res.send(result);
-   })
+    });
 
-   // mangage classes
-   app.get('/classes-manage', async (req, res) => {
+    // Get class by instructor email address
+    app.get('/classes/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { instructorEmail: email };
+      const result = await classesCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // manage classes
+    app.get('/classes-manage', async (req, res) => {
       const result = await classesCollections.find().toArray();
       res.send(result);
-   })
+    });
 
-   // update classes
-   app.put('/change-status', async(req, res) => {
+    // update classes
+    app.put('/change-status', async (req, res) => {
       const id = req.body.id;
       const status = req.body.status;
       const reason = req.body.reason;
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           status: status,
           reason: reason
         }
-      }
+      };
       const result = await classesCollections.updateOne(filter, updateDoc, options);
       res.send(result);
+    });
 
-   })
-
-   // Get classes aproved
-   app.get('/approved-classes', async (req, res) => {
-      const query = {status: "approved"};
+    // Get classes approved
+    app.get('/approved-classes', async (req, res) => {
+      const query = { status: "approved" };
       const result = await classesCollections.find(query).toArray();
       res.send(result);
-   })
+    });
 
-   // Get the singles classes
-   app.get('/classes/:id', async (req, res) => {
+    // Get the single class
+    app.get('/classes/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await classesCollections.findOne(query);
       res.send(result);
-   })
+    });
 
-   // update class details 'all data
-
-   app.put('/update-class/:id', async (req, res) => {
-      const id = req.parms.id;
+    // update class details 'all data
+    app.put('/update-class/:id', async (req, res) => {
+      const id = req.params.id;
       const updateClass = req.body;
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           name: updateClass.name,
@@ -120,10 +119,10 @@ async function run() {
           videoLink: updateClass.videoLink,
           status: 'approved',
         }
-      }
+      };
       const result = await classesCollections.updateOne(filter, updateDoc, options);
       res.send(result);
-   })
+    });
 
     // Lancer le serveur après la connexion réussie à la base de données
     app.get('/', (req, res) => {
