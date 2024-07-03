12ff9d0122d9d457d7a56032ef7b4059904ecf5f
unknown <elhadjoumartall10@gmail.com>
2024-07-03 20:50:29 +0200

    Creation des fichier router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
+import { RouterProvider } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 import 'react-toastify/dist/ReactToastify.css';
 import Aos from 'aos';
-import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
+import { router } from './routes/router';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello, Developers!</div>}
-]);
+
+
 
 Aos.init();
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    
+  <QueryClientProvider client={queryClient}>
+  <RouterProvider router={router} />
+  </QueryClientProvider>
 )
