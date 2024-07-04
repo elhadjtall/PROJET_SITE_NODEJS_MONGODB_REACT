bfd11239fa6117a9499290341883c2b05dc59372
unknown <elhadjoumartall10@gmail.com>
2024-07-04 13:17:33 +0200

    Creation des fichier du navbar

---
+++ frontend/src/components/headers/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navLinks = [
+    {
+        name: "Home",
+        route: "/"},
+    {name: 'Instructors', route:'/instructors'},
+    {name: 'Classes', route:'/classes'},
+
+    ]
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/headers/Navbar'
 
 const MainLayout = () => {
   return (
-    <div>
-        <nav>Navbar</nav>
+    <main className="dark:bg-black overflow-hidden">
+        <Navbar/>
         <Outlet/>
         <footer>Footer</footer>
-    </div>
+    </main>
   )
 }
 
