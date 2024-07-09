fb3e38c9a61845471b1192f1c97c0b9a1d352e18
unknown <elhadjoumartall10@gmail.com>
2024-07-09 16:14:56 +0200

    AJout des images backroung

--- frontend/src/pages/Home/Hero/Hero.jsx
+++ frontend/src/pages/Home/Hero/Hero.jsx
@@ -6,7 +6,23 @@ const Hero = () => {
     <div 
       className="min-h-screen bg-cover" 
       style={{ backgroundImage: `url(${bgImg})` }}>
-        <div></div>
+        <div className='min-h-screen flex justify-start pl-11 items-center text-white bg-black bg-opacity-60'>
+          <div>
+            <div className='space-y-4'>
+              <p className='md:text-4xl text-2xl'>We Provider</p>
+              <h1 className='md:text-7xl text-4xl font-bold'>Best Yoga Course Online</h1>
+              <div className='md:w-1/2'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem dicta necessitatibus ad est itaque impedit, 
+                  placeat dolorum, iure perspiciatis ab voluptatum voluptates! Ipsam vero magnam saepe veritatis sequi ab. Nam.</p>
+              </div>
+              <div className='flex flex-wrap items-center gap-5'>
+                <button className='px-7 py-3 rounded-lg bg-secondary font-bold uppercase'>Join TOday</button>
+                <button className='px-7 py-3 rounded-lg border font-bold uppercase'>Wiew Course</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
 
       </div>
   );
--- frontend/src/pages/Home/Hero/Hero2.jsx
+++ frontend/src/pages/Home/Hero/Hero2.jsx
@@ -7,13 +7,13 @@ const Hero2 = () => {
       className="min-h-screen bg-cover bg-center" 
       style={{ backgroundImage: `url(${anotherBgImg})` }}
     >
-      {/* <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-50">
-        <div className="text-center text-white">
-          <h1 className="text-5xl font-bold mb-4">Discover More</h1>
-          <p className="text-lg">Join our classes and learn from the best instructors.</p>
-        </div>
-      </div> */}
-    </div>
+    //   {/* <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-50">
+    //     <div className="text-center text-white">
+    //       <h1 className="text-5xl font-bold mb-4">Discover More</h1>
+    //       <p className="text-lg">Join our classes and learn from the best instructors.</p>
+    //     </div>
+    //   </div> */}
+    // </div>
   );
 };
 
