3ce60021619d40c764ed501febd159f8b0bdb282
unknown <elhadjoumartall10@gmail.com>
2024-07-09 17:04:13 +0200

    Ajout du bloc images gallery

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import HeroContainer from './Hero/HeroContainer';
+import Gallary from './Gallary/Gallary';
 
 const Home = () => {
   return (
     <section>
       <HeroContainer />
 
-      <div>
-        
+      <div className='mex-w-screen-xl mx-auto'>
+        <Gallary/>
+
       </div>
     </section>
   );
