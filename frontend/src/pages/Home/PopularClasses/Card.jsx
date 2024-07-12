72754e99946b377c8fed952c24abc9467e4db85b
unknown <elhadjoumartall10@gmail.com>
2024-07-12 12:02:11 +0200

    Ajout des images Cards

--- frontend/src/pages/Home/PopularClasses/Card.jsx
+++ frontend/src/pages/Home/PopularClasses/Card.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 // import './Card.css';
 
 const Card = ({ item }) => {
+    console.log(item);
+    const {_id, name, image, availableSeats, price, totalEnrolled} = item;
+
     return (
-        <div className="card">
-            Card
-            {/* <h2 className="card-title">{item.title}</h2>
-            <p className="card-description">{item.description}</p> */}
+        <div className="shadow-lg rounded-lg p-3 flex flex-col justify-between border border-secondary overflow-hidden m-4">
+            <img src={image} alt="" />
+            <div className="p-4">
+                <h2 className='text-xl font-semibold mb-2 dark:text-white text-center'>{name}</h2>
+                <p className="text-gray-600 mb-2">{availableSeats} available seats</p>
+                <p className="text-gray-600 mb-2">Price: {price}</p>
+                <p className="text-gray-600 mb-2">Total Students: {totalEnrolled}</p>
+                <button className="bg-secondary text-white p-2 rounded-lg">View Details</button>
+                
+            </div>
         </div>
     );
 };
--- frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
+++ frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import useAxiosFetch from '../../../hooks/useAxiosFetch';
 import Card from './Card';  // Assurez-vous que le chemin est correct
 
+
+{/* C'est const qui permet de recuperer les images du Data et les afficher  */}
 const PopularClasses = () => {
     const axiosFetch = useAxiosFetch();
     const [classes, setClasses] = useState([]);
@@ -21,7 +23,10 @@ const PopularClasses = () => {
     }, [axiosFetch]);
 
     return (
-        <div className='md:w-[80%] mx-auto my-28'>
+
+        <div className='md:w-[80%] mx-auto my-28'> {/* c'est le div principal qui contient toutes les images */}
+
+            {/* Titre et le texte */}
             <div>
                 <h1 className='text-5xl font-bold text-center'>
                     Our <span className='text-secondary'>Popular</span> Classes
@@ -33,6 +38,7 @@ const PopularClasses = () => {
                     </p>
                 </div>
             </div>
+            {/* Affichage des images et Cards  */}
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     classes.map((item, index) => <Card key={index} item={item} />)
