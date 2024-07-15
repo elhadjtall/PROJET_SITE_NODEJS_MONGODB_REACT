77446e4c0524af7099d08c1921828f0c1b15df32
unknown <elhadjoumartall10@gmail.com>
2024-07-15 23:47:56 +0200

    AJout du code backend

--- frontend/src/pages/Home/PopularTeacher/PopularTeacher.jsx
+++ frontend/src/pages/Home/PopularTeacher/PopularTeacher.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import useAxiosFetch from '../../../hooks/useAxiosFetch';
+
+import img from '../../../assets/home/girl.jpg';
 
 const PopularTeacher = () => {
-    const [instructors, setInstructors] = useState([]);
-    const axiosFetch = useAxiosFetch();
+  const [instructors, setInstructors] = useState([]);
+  const axiosFetch = useAxiosFetch();
 
-    useEffect(() => {
-        axiosFetch
-          .get("")
-    }, [axiosFetch]);
+  useEffect(() => {
+    axiosFetch
+      .get("/popular-instructors")
+      .then((response) => {
+        setInstructors(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(instructors);
   return (
     <div className='md:w-[80%] mx-auto my-28'> {/* c'est le div principal qui contient toutes les images */}
 
@@ -25,6 +36,7 @@ const PopularTeacher = () => {
             </div>
             {/* Affichage des images et Cards  */}
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                
                 
             </div>
         </div>
