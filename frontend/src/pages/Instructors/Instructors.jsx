b3c9141271f1eee9772de38cce3cd4706ba45d21
unknown <elhadjoumartall10@gmail.com>
2024-07-18 17:28:41 +0200

    Ajout de la page Instrucotors

--- frontend/src/pages/Instructors/Instructors.jsx
+++ frontend/src/pages/Instructors/Instructors.jsx
@@ -32,20 +32,21 @@ const Instructors = () => {
         </div>
       </div>
       {instructors.length > 0 ? (
-        <div className='grid mb-28 md:grid-cols-2 lg:grid-cols-4 w-[90%] gap-4 mx-auto'>
+        <div className='grid mb-28 md:grid-cols-2 lg:grid-cols-4 w-[90%] gap-4 mx-auto mt-20'>
           {instructors.slice(0, 6).map((instructor, i) => (
             <div key={i} className='flex dark:text-white hover:-translate-y-2 duration-200 cursor-pointer flex-col shadow-md py-8 px-10 md:px-8 rounded-md'>
               <div className='flex-col flex gap-6 md:gap-8'>
                 <img
                   className='rounded-full border-4 border-gray-300 h-24 w-24 mx-auto'
                   src={instructor?.photoUrl || img}
-                  alt={instructor?.instructor?.name || 'Instructor'}
+                  alt=''
                 />
               </div>
               <div className='flex flex-col text-center'>
                 <p className='font-medium text-lg dark:text-white text-gray-800'>{instructor?.instructor?.name}</p>
-                <p className='text-gray-500 whitespace-nowrap'>Instructor</p>
-                <p className='text-gray-500 mb-4 whitespace-nowrap'>Total Students: {instructor?.totalEnrolled}</p>
+                <p className='text-gray-500'>Instructor</p>
+                <p className='text-gray-500 mb-4'> Adresses: {instructor?.address}</p>
+                <p className='text-gray-500 mb-4'> Email: {instructor?.email}</p>
               </div>
             </div>
           ))}
